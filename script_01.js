a5818582b4b7bb206887b8743c2670a9c7e939e3
Tinneff <122363546+Tinneff@users.noreply.github.com>
2023-01-16 12:48:22 +0100

    Update script_01.js

--- script_01.js
+++ script_01.js
@@ -9,6 +9,10 @@ const ageMark = 30;
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
@@ -16,21 +20,33 @@ console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
 console.log("------------------");
 
-
-/************ IF  ************/
-// TINA --> There is no alternative!
-// entweder JA oder nix ... alternativlos
-
 /************ IF  ************/
 // TINA --> There is no alternative!
 // entweder JA oder nix ... alternativlos
 
-if (true)
+// if (true && true)
+// if (isJohnOlder)  
+// if (ageJohn > ageMark)
+// {
+//     console.log("John ist älter."); 
+// }
 
-{
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
 
-    console.log ("Johne ist älter");
-}
+// if (true) 
+// if (false) 
+// if (isJohnOlder)
+// if (ageJohn > ageMark)
+// {
+//     console.log("John ist älter."); 
+// } 
+// else 
+// {
+//     console.log("John ist jünger.");  
+// }
 
-
+/************  Ternäre (ternary) Schreibweise ************/
+console.log( (isJohnOlder) ? "John is older" : "Mark is older");
 
